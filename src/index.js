5e58bfc82d0f3fe1277cf10602b120d0ffd47a7d
Breanna Henriquez <50420371+B-Hen@users.noreply.github.com>
2021-02-07 16:02:17 -0500

    Finsihed Random-Joke-With-Tooling-Checkoff

--- src/index.js
+++ src/index.js
@@ -24,16 +24,16 @@ const errorPage = `
 
 // array of jokes
 const jokes = [
-  { q: 'What do you call a very small valentine?', a: 'A valen-tiny! :)' },
-  { q: 'What did the dog say when he rubbed his tail on the sandpaper?', a: 'Ruff, Ruff! :)' },
-  { q: "Why don't sharks like to eat clowns?", a: 'Because they taste funny! :)' },
-  { q: 'What did the boy cat say to the girl cat?', a: "You're Purr-fect! :)" },
-  { q: "What is a frog's favorite outdoor sport?", a: 'Fly Fishing! :)' },
-  { q: 'I hate jokes about German sausages.', a: 'Theyre the wurst. :)' },
-  { q: 'Did you hear about the cheese factory that exploded in France?', a: 'There was nothing left but de Brie. :)' },
-  { q: 'Our wedding was so beautiful ', a: 'Even the cake was in tiers. :)' },
-  { q: 'Is this pool safe for diving?', a: 'It deep ends. :)' },
-  { q: 'Dad, can you put my shoes on?', a: 'I dont think theyll fit me. :)' },
+  { q: 'What do you call a very small valentine?', a: 'A valen-tiny! >:)' },
+  { q: 'What did the dog say when he rubbed his tail on the sandpaper?', a: 'Ruff, Ruff! >:)' },
+  { q: "Why don't sharks like to eat clowns?", a: 'Because they taste funny! >:)' },
+  { q: 'What did the boy cat say to the girl cat?', a: "You're Purr-fect! >:)" },
+  { q: "What is a frog's favorite outdoor sport?", a: 'Fly Fishing! >:)' },
+  { q: 'I hate jokes about German sausages.', a: 'Theyre the wurst. >:)' },
+  { q: 'Did you hear about the cheese factory that exploded in France?', a: 'There was nothing left but de Brie. >:)' },
+  { q: 'Our wedding was so beautiful ', a: 'Even the cake was in tiers. >:)' },
+  { q: 'Is this pool safe for diving?', a: 'It deep ends. >:)' },
+  { q: 'Dad, can you put my shoes on?', a: 'I dont think theyll fit me. >:)' },
 ];
 
 // function to get jokes
